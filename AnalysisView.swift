8939db35349c724bdb4beabaa95ef508281b6829
tongxi.zhang <422853495@qq.com>
2016-01-13 11:01:57 +0800

    change something

--- AnalysisView.swift
+++ AnalysisView.swift
@@ -2,37 +2,13 @@
 //  AnalysisView.swift
 //  ShuaTi
 //
-//  Created by zhongjchange on 15/4/20.
+//  Created by zhangtongxi on 15/4/20.
 //  Copyright (c) 2015年 Dream. All rights reserved.
 //
 
 import UIKit
 
 class AnalysisView: UIView {
-
-    @IBOutlet weak var answerDetail: UILabel!
-    
-    @IBOutlet weak var questionDetail: UILabel!
-    
-    @IBOutlet weak var analysisLabel: UILabel!
-    
-    @IBOutlet weak var analysisContent: LmlView!
-    
-    @IBOutlet weak var chapterLabel: UILabel!
-    
-    @IBOutlet weak var chaptersCollection: UICollectionView!
-    
-    @IBOutlet weak var noteLabel: UILabel!
-    
-    @IBOutlet weak var noteButton: UIButton!
-    
-    @IBOutlet weak var noteContent: UILabel!
-    
-    @IBOutlet weak var noteImagesTable: UITableView!
-    
-    @IBOutlet var verticalSpaces: [NSLayoutConstraint]!
-    
-    private let bottomSpace: CGFloat = 120
     
     var realHeight: CGFloat {
         get {
